33ed5c2b253fd04f495a1a7e8af756d0f98b9c10
fancn21th <fancn21th@gmail.com>
2024-02-02 19:00:44 +0800

    🐛 FIX: unused import

--- src/hooks/useRecognition.ts
+++ src/hooks/useRecognition.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { startRecognition, endRecognition } from "../utils";
 import type { RecognitionState } from "../utils";
 
